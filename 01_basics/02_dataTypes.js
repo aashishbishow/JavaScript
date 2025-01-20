9cbf329bffa5eb36d9d7209697dfdb542522c686
Aashish BishowKarma <aashishbishowkarma@outlook.com>
2025-01-20 12:09:43 +0000

    datatypes in JavaScript

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict";
+// Treated all js code as newer version of JavaScript
+
+// alert(3 +3); we are using nodejs not browser so it can;t be run in nodejs runtime
+
+console.log(3
+    +3) //code readability should be high, it is not a good practise
+console.log("Aashish")
+
+
+let name = "Aashish";
+let age = 23;
+let isLoggedIn = false;
+let stat = null;
+
+// Primitive datatypes
+// number => 2 to power 53:range
+// bigInt
+// string => ""
+// bollean => true or false
+// null => datatype and standalone value in js, representation of empty value in js, null is a object in javascript
+// undefined => when value is not nefined
+// symbol => finding uniqueness, mostly implenentaion in react, defining uniqueness
+
+// Objects
+
+console.log(typeof "Aashish"); // typeof => finding the datatype of value or variable
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object, also laguage subpoint
+
