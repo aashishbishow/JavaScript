f8da14c5b21be4606b203f1e02b4aa3636940285
Aashish BishowKarma <aashishbishowkarma@outlook.com>
2025-01-20 16:45:07 +0000

    03 conversion operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -32,3 +32,30 @@ let stringNumber = String(someNumber); // number is converted into string
 
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+// ******************* Operations *******************
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2**3);
+// console.log(2%3);
+
+let str1 = "hello";
+let str2 = " Aashish";
+
+let str3 = str1 +str2 ;
+
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
